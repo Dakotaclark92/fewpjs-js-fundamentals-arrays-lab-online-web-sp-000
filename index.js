5ac48366fcae86bbc797bd8f59a42e20dfa2a423
Dakota Clark <dakotaclark92@yahoo.com>
2019-11-30 03:52:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const append = ['Milo', 'Otis', 'Garfield'];
 append.push('Otis');
 
-const prepend = ["Milo", "Otis", "Garfield"];
+const prepend = ['Milo', 'Otis', 'Garfield'];
 
-const removeLast = ["Milo", "Otis", "Garfield"];
+const removeLast = ['Milo', 'Otis', 'Garfield'];
 
-const removeFirst = ["Milo", "Otis", "Garfield"];
+const removeFirst = ['Milo', 'Otis', 'Garfield'];
